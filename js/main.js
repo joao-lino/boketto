d18b8965ca5a7136e774408630d84abcf31cece1
joao-arch <64785287+joao-arch@users.noreply.github.com>
2022-09-29 21:15:47 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,73 @@
+function search_article() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('card');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
+
+const readingProgress = document.querySelector('#reading-progress-fill');
+document.addEventListener('scroll', function(e) {
+  let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+  readingProgress.style.setProperty('width', w + '%');
+});
+
+//darkmode
+
+const toggleBtn = document.getElementById("toggle-btn");
+const theme = document.getElementById("body");
+let darkMode = localStorage.getItem("dark-mode");
+var logo = document.getElementById("main-logo");
+var themeIcon = document.getElementById("sun");
+
+const enableDarkMode = () => {
+  theme.classList.add("dark-mode-theme");
+  toggleBtn.classList.remove("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "enabled");
+  logo.src = "icons/bokettoiconwhite.svg";
+  themeIcon.src = "./icons/moon.svg";
+};
+
+const disableDarkMode = () => {
+  theme.classList.remove("dark-mode-theme");
+  toggleBtn.classList.add("dark-mode-toggle");
+  localStorage.setItem("dark-mode", "disabled");
+  logo.src="icons/bokettoicon.svg";
+  themeIcon.src = "./icons/sun.svg";
+   
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+
+  // set state of darkMode on page load
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
